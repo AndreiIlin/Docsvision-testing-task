381a2fb548aa99a2e9fcb079c834af62590975d0
AndreiIlin <andrei362065@gmail.com>
2024-05-31 14:57:16 +0300

    feat: Add env base path

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.VITE_APP_BASE_PATH}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
